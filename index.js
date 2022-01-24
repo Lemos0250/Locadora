aa7193c9a66d0cd849142c46bdcfeb93f4afa673
Lemos0250 <vitorlemos703@gmail.com>
2022-01-24 20:29:44 -0300

    Adição de Novos Códigos

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
-"use strict";
 (function () {
     var _a;
     const $ = (query) => document.querySelector(query);
     //resume o domcumento .query Selector Evitando utilizar várias vezes o Seletor. Retorna um elemento ou Nulo
+    function calcTempo(mil) {
+        const min = Math.floor(mil / 60000);
+        const sec = Math.floor((mil % 60000) / 1000);
+        return `${min}m e ${sec}s`;
+    }
     function locadora() {
-        function ler() { }
-        function adicionar(Filme) {
-            var _a;
+        function ler() {
+            return localStorage.locadora ? JSON.parse(localStorage.locadora) : [];
+        }
+        function salvar(Filme) {
+            localStorage.setItem("locadora", JSON.stringify(Filme));
+        }
+        function adicionar(Filme, salva) {
+            var _a, _b;
             const row = document.createElement("tr");
             row.innerHTML = `
         <td>${Filme.nome_filme}</td>
         <td>${Filme.nome_pessoa}</td>
         <td>${Filme.alugado}</td>
         <td>
-        <button class="delete" data-aluguel="${Filme.nome_filme}">X</button>
+         <button class="delete" data-alugado="${Filme.nome_filme}">X</button>
         </td>
         `;
-            (_a = $("#locadora")) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+            (_a = row.querySelector(".delete")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+                remover(this.dataset.nome_filme);
+            });
+            (_b = $("#locadora")) === null || _b === void 0 ? void 0 : _b.appendChild(row);
+            if (salva)
+                salvar([...ler(), Filme]);
         }
-        function remover() { }
-        function salvar() { }
-        function render() { }
+        function remover(nome_filme) {
+            const { alugado, nome_pessoa } = ler().find((Filme) => Filme.nome_filme === nome_filme);
+            const tempo = calcTempo(new Date().getTime() - new Date(alugado).getTime());
+            if (!confirm(`O filme ${nome_filme} está alugado a ${tempo} Deseja Devolver?`))
+                return;
+            salvar(ler().filter((Filme) => Filme.nome_filme !== nome_filme));
+            render();
+        }
+        function render() {
+            $("#locadora").innerHTML = "";
+            const locadora = ler();
+            if (locadora.length) {
+                locadora.forEach((Filme) => adicionar(Filme));
+            }
+        }
         return { ler, adicionar, remover, salvar, render };
     }
+    locadora().render();
     (_a = $("#cadastrar")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
         var _a, _b;
         const nome_pessoa = (_a = $("#nome_pessoa")) === null || _a === void 0 ? void 0 : _a.value;
@@ -31,6 +58,6 @@
             alert("Obrigatório o Seu Nome e o Filme Escolhido para Alugar, Favor insira corretamente os Dados");
             return;
         }
-        locadora().adicionar({ nome_pessoa, nome_filme, alugado: new Date });
+        locadora().adicionar({ nome_pessoa, nome_filme, alugado: new Date().toISOString() }, true);
     });
 })();
--- index.ts
+++ index.ts
@@ -1,17 +1,30 @@
-interface Filme {
+interface filme {
     nome_pessoa: string;
     nome_filme: string;
-    alugado: Date;
+    alugado: Date | string;
 }
 
 (function() { 
     const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
     //resume o domcumento .query Selector Evitando utilizar várias vezes o Seletor. Retorna um elemento ou Nulo
 
-    function locadora(){
-        function ler(){}
+    function calcTempo(mil: number) {
+        const min = Math.floor(mil / 60000);
+        const sec = Math.floor((mil % 60000) / 1000);
 
-        function adicionar(Filme: Filme){
+        return `${min}m e ${sec}s`;
+    }
+
+    function locadora(){
+        function ler(): filme[] {
+            return localStorage.locadora ? JSON.parse(localStorage.locadora) : [];
+        }
+
+        function salvar(Filme: filme[]) {
+            localStorage.setItem("locadora", JSON.stringify(Filme));
+        }
+
+        function adicionar(Filme: filme, salva?: boolean){
         const row = document.createElement("tr");
 
         row.innerHTML = `
@@ -19,30 +32,57 @@ interface Filme {
         <td>${Filme.nome_pessoa}</td>
         <td>${Filme.alugado}</td>
         <td>
-        <button class="delete" data-aluguel="${Filme.nome_filme}">X</button>
+         <button class="delete" data-alugado="${Filme.nome_filme}">X</button>
         </td>
         `;
 
+        row.querySelector(".delete")?.addEventListener("click", function() {
+            remover(this.dataset.nome_filme);
+        });
+
          $("#locadora")?.appendChild(row);
+
+         if (salva) salvar([...ler(), Filme]);
         }
-        function remover(){}
 
-        function salvar(){}
+        function remover(nome_filme: string) {
 
-        function render(){}
+            const { alugado, nome_pessoa} = ler().find((Filme) => Filme.nome_filme === nome_filme);
 
-        return{ler,adicionar,remover,salvar,render};
+            const tempo = calcTempo(
+                new Date().getTime() - new Date(alugado).getTime());
+
+            if (
+                !confirm(`O filme ${nome_filme} está alugado a ${tempo} Deseja Devolver?`))
+            return;
+
+            salvar(ler().filter((Filme) => Filme.nome_filme !== nome_filme));
+            render();
+        }
+
+        function render() {
+            $("#locadora")!.innerHTML = "";
+            const locadora = ler();
+
+            if(locadora.length) {
+                locadora.forEach((Filme) => adicionar(Filme));
+            }
+        }
+
+        return{ ler, adicionar, remover, salvar, render };
     }
+
+    locadora().render();
 
     $("#cadastrar")?.addEventListener("click", () => {
         const nome_pessoa = $("#nome_pessoa")?.value;
         const nome_filme = $("#nome_filme")?.value;
         
-        if(!nome_pessoa || !nome_filme) {
+        if (!nome_pessoa || !nome_filme) {
             alert("Obrigatório o Seu Nome e o Filme Escolhido para Alugar, Favor insira corretamente os Dados");
             return;
         }
 
-        locadora().adicionar({ nome_pessoa, nome_filme, alugado: new Date});
+        locadora().adicionar({ nome_pessoa, nome_filme, alugado: new Date().toISOString() }, true);
     });
 })();
